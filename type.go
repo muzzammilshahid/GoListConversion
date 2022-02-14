fd42affa57eaa5732c74386d5f6c88eb52a7b7b3
muzzammilshahid <muzzammilshahid50@gmail.com>
2022-02-14 16:26:54 +0500

    remove extra code

--- type.go
+++ type.go
@@ -11,63 +11,6 @@ import (
 
 func main() {
 
-	primes := []string{"2", "3", "test", "7", "11", "false"}
-
-	test1, err2 := json.Marshal(primes)
-
-	if err2 != nil {
-		fmt.Println(err2)
-	} else {
-		//fmt.Println("here")
-		//fmt.Println(&test1[0])
-	}
-
-	var test []interface{}
-
-	error2 := json.Unmarshal(test1, &test)
-
-	if error2 != nil {
-		fmt.Println(error2)
-	}
-
-	//fmt.Println(test)
-	//for i := range test {
-	//	fmt.Println(test[i])
-	//}
-
-	testing := [7]string{"2", "3", "test", "7", "1.11", "false", "{\"id\":\"1\"}"}
-
-	var testAfter [len(testing)]interface{}
-
-	for i := range testing {
-		value := testing[i]
-		number, errNumber := strconv.Atoi(value)
-		float, errFloat := strconv.ParseFloat(value, 64)
-		boolean, errBoolean := strconv.ParseBool(value)
-		if errNumber == nil {
-			testAfter[i] = number
-		} else if errFloat == nil {
-			testAfter[i] = float
-		} else if errBoolean == nil {
-			testAfter[i] = boolean
-		} else if isValidJSON(value) {
-			var jsonMap map[string]interface{}
-			data, err := getBytes(value)
-			if err != nil {
-				panic(err.Error())
-			}
-			data = data[4:]
-			err = json.Unmarshal(data, &jsonMap)
-			testAfter[i] = jsonMap
-		} else {
-			testAfter[i] = testing[i]
-		}
-	}
-
-	//for i := range testAfter {
-	//	fmt.Println("value =", testAfter[i], ", type =", reflect.TypeOf(testAfter[i]))
-	//}
-
 	testingFunction := []string{"2", "3", "test", "7", "1.11", "false", "{\"number\":1356,\"id\":\"1\",\"name\":\"test\"}"}
 
 	for i := range listToTypedList(testingFunction) {
