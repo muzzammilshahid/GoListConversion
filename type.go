8877a477dc9430df8a62c076ad7d0e99c25a2395
muzzammilshahid <muzzammilshahid50@gmail.com>
2022-02-14 15:24:55 +0500

    add json

--- type.go
+++ type.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
 	"encoding/json"
 	"fmt"
 	"reflect"
@@ -33,7 +35,7 @@ func main() {
 	//	fmt.Println(test[i])
 	//}
 
-	testing := [6]string{"2", "3", "test", "7", "1.11", "false"}
+	testing := [7]string{"2", "3", "test", "7", "1.11", "false", "{\"id\":\"1\"}"}
 
 	var testAfter [len(testing)]interface{}
 
@@ -48,6 +50,15 @@ func main() {
 			testAfter[i] = float
 		} else if errBoolean == nil {
 			testAfter[i] = boolean
+		} else if isValidJSON(value) {
+			var jsonMap map[string]interface{}
+			data, err := getBytes(value)
+			if err != nil {
+				panic(err.Error())
+			}
+			data = data[4:]
+			err = json.Unmarshal(data, &jsonMap)
+			testAfter[i] = jsonMap
 		} else {
 			testAfter[i] = testing[i]
 		}
@@ -57,7 +68,7 @@ func main() {
 	//	fmt.Println("value =", testAfter[i], ", type =", reflect.TypeOf(testAfter[i]))
 	//}
 
-	testingFunction := []string{"2", "3", "test", "7", "1.11", "false"}
+	testingFunction := []string{"2", "3", "test", "7", "1.11", "false", "{\"number\":1356,\"id\":\"1\",\"name\":\"test\"}"}
 
 	for i := range listToTypedList(testingFunction) {
 		fmt.Println("value =", listToTypedList(testingFunction)[i], ", type =", reflect.TypeOf(listToTypedList(testingFunction)[i]))
@@ -75,10 +86,40 @@ func listToTypedList(args []string) []interface{} {
 			convertedList = append(convertedList, float)
 		} else if boolean, errBoolean := strconv.ParseBool(value); errBoolean == nil {
 			convertedList = append(convertedList, boolean)
+		} else if isValidJSON(value) {
+			var jsonMap map[string]interface{}
+			data, err := getBytes(value)
+			if err != nil {
+				panic(err.Error())
+			}
+			data = data[4:]
+			err = json.Unmarshal(data, &jsonMap)
+			convertedList = append(convertedList, jsonMap)
 		} else {
 			convertedList = append(convertedList, value)
 		}
 	}
 
 	return convertedList
+}
+
+func isValidJSON(i interface{}) bool {
+	var str map[string]interface{}
+	data, err := getBytes(i)
+	if err != nil {
+		panic(err.Error())
+	}
+	data = data[4:]
+	err = json.Unmarshal(data, &str)
+	return err == nil
+}
+
+func getBytes(i interface{}) ([]byte, error) {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(i)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
 }
